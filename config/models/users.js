aaf84b73e8cb199736ef7c7990a8892b98d04272
ahernd2 <david.ahern@dell.com>
2017-11-12 17:48:51 +0000

    Added redis docker

--- config/models/users.js
+++ config/models/users.js
@@ -2,9 +2,6 @@
 
 module.exports = {
   properties: {
-    firstname: {isRequired: true, isMutable: true},
-    lastname: {isRequired: true, isMutable: true},
-    email: {isRequired: false, isMutable: true},
-    redirectUri: {isRequired: false, isMutable: true}
+    email: {isRequired: false, isMutable: true}
   }
 };
